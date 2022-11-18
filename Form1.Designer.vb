1e1d98f3409e24d494794ee77b4142d13086b82e
AARON ADONIS VELASCO <adonis12899@gmail.com>
2022-11-18 13:37:40 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class shuffler
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_shuffler = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btn_shuffler
+        '
+        Me.btn_shuffler.Location = New System.Drawing.Point(47, 333)
+        Me.btn_shuffler.Name = "btn_shuffler"
+        Me.btn_shuffler.Size = New System.Drawing.Size(182, 55)
+        Me.btn_shuffler.TabIndex = 0
+        Me.btn_shuffler.Text = "START"
+        Me.btn_shuffler.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 72)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(255, 116)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 191)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "WINNERS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "LIST"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(129, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(141, 22)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(74, 6)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "QTY : "
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(12, 211)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(255, 116)
+        Me.ListBox2.TabIndex = 4
+        '
+        'shuffler
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
+        Me.ClientSize = New System.Drawing.Size(282, 397)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btn_shuffler)
+        Me.Name = "shuffler"
+        Me.Text = "Randomizer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_shuffler As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox2 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class shuffler
+    Dim names As List(Of String)
+
+
+
+
+    Private Sub shuffler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ListBox1.Items.Add("Canada")
+        ListBox1.Items.Add("USA")
+        ListBox1.Items.Add("UK")
+        ListBox1.Items.Add("Japan")
+        ListBox1.Items.Add("Russia")
+        ListBox1.Items.Add("China")
+        ListBox1.Items.Add("India")
+    End Sub
+
+    Private Sub btn_shuffler_Click(sender As Object, e As EventArgs) Handles btn_shuffler.Click
+
+        'Dim rand As New Random
+        'Dim index As Integer
+        'Dim item As Object
+
+        ''Display the items in random order.
+
+        'Dim limiter As Integer = ListBox1.Items.Count - TextBox1.Text
+        'While ListBox1.Items.Count > limiter ' << ----- SET LIMITS
+        '    index = rand.Next(0, ListBox1.Items.Count)
+        '    item = ListBox1.Items(index)
+        '    'item deleter
+        '    ListBox1.Items.RemoveAt(index)
+        '    ListBox2.Items.Add(item)
+        'End While
+        'ListBox2.Items.Clear()
+        Dim qty As Integer = TextBox1.Text
+        Dim cou As Integer = 0
+        Dim ls As List(Of String) = List_to_array(ListBox1)
+        Dim ran As List(Of String) = Randomizer(ls, qty)
+        While ran.Count > cou
+            ListBox1.Items.Add(ran.Item(cou).ToString)
+            cou = +1
+        End While
+    End Sub
+
+
+    ' randomizer return list of winners by aaron 
+    ' calling method 
+    ' ex   Randomizer(<list of String>, Quantity) 
+    Public Function Randomizer(ByVal list As List(Of String), ByVal qty As Integer) As List(Of String)
+        Dim rand As New Random
+        Dim index As Integer
+        Dim namelist As New List(Of String)
+        Dim limiter As Integer = list.Count - qty
+
+
+        While list.Count > limiter ' << ----- SET LIMITS
+            index = rand.Next(0, list.Count)
+            namelist.Add(list.Item(index).ToString)
+            'item deleter'
+            list.RemoveAt(index)
+
+        End While
+
+        Return namelist
+
+    End Function
+
+
+    Public Function List_to_array(ByVal list As ListBox) As List(Of String)
+        Dim lss As List(Of String) = New List(Of String)
+        Dim counter As Integer = 0
+        Dim it As Object
+        While list.Items.Count > counter
+
+            it = list.Items(counter)
+            lss.Add(it)
+            counter = +1
+        End While
+        Return lss
+    End Function
+
+
+End Class
